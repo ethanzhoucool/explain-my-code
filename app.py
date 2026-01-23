128f3a8f2564f368f923708c64b741c765967248
Ethan Zhou <ethan.st.zhou@gmail.com>
2026-01-23 17:06:14 -0500

    Add Render deployment files

--- app.py
+++ app.py
@@ -32,4 +32,6 @@ def explain():
                          level=level)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
